5d28bbe14b5bc067ae40d27ce112c91bfa33bbec
Vitor Bonfim <vitzxzz@gmail.com>
2026-08-19 10:34:00 -0300

    Exercícios da aula comentados
    Falta responder, apenas comentei de uma forma legivel para ler

--- SEGUNDO SEMESTRE/Programação imperativa/Aula 19-08/main.cpp
+++ SEGUNDO SEMESTRE/Programação imperativa/Aula 19-08/main.cpp
@@ -1,5 +1,57 @@
 #include <stdio.h>
 
+
+
+//EXERCÍCIOS DA AULA DE PROGRAMAÇÃO IMPERATIVA
+
+
+
+//IF ELSE:
+
+//(a) Escreva um programa na linguagem C que, dados dois números inteiros distintos fornecidos pelo usuário (via
+//teclado), imprima-os em ordem crescente.
+//Obs:
+//O programa deve certificar que os dois números sejam distintos entre si.
+//O programa não pode empregar nenhum comando de repetição, nem armazenar dados em array.
+//(b) Escreva um programa na linguagem C que, dadas três letras distintas fornecidas pelo usuário (via teclado),
+//imprima-as em ordem alfabética.
+//Obs:
+//O programa deve certificar que as três letras sejam distintas entre si.
+//O programa não pode empregar nenhum comando de repetição, nem armazenar dados em array.
+
+
+
+//SWITCH:
+
+//Escreva um programa em C para simular uma calculadora com as quatro operações básicas (adição, subtração,
+//multiplicação e divisão). O programa deve receber, como entrada, dois operandos (valores do tipo double) e um
+//operador (um valor do tipo char). Como saída, o programa deve exibir o resultado da aplicação do operador sobre os
+//dois operandos.
+//Obs:
+//O programa deve empregar o comando switch para decidir qual das quatro operações deve ser realizada.
+//O programa deve impedir a tentativa de divisão por zero.
+
+
+
+//WHILE - DO WHILE - FOR
+
+//a) Sendo H = 1 + 1/2 + 1/3 + ¼ + ... 1/N, escreva um programa em C para gerar o número H.
+//O valor de N deve ser fornecido pelo usuário. Use a estrutura for para somar a sequência de termos.
+//(b) Escreva um programa em C para calcular o fatorial de um número N fornecido pelo usuário.
+//Use a estrutura for para gerar a sequência de termos.
+//(c) Escreva um programa em C para calcular o valor da série S abaixo.
+//O valor de N deve ser fornecido pelo usuário. Use a estrutura do-while para somar a sequência de termos.
+//S = 1/N + 2/N-1 + 3/N-2 + ...+ N-1/2 + N/1
+//(d) Escreva um programa em C para imprimir os 30 primeiros números naturais pares.
+//Use a estrutura for para gerar a sequência de termos.
+
+
+
+//CONST - ENUM - TYPEDEF
+
+//(a) Defina um tipo enumerado para representar 10 moedas diferentes (real, dólar, libra, etc).
+//(b) Defina um tipo enumerado para representar 10 línguas diferentes (português, alemão, francês, etc).
+
 int main()
 {
 
