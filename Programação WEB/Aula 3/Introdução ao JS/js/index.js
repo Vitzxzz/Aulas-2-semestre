9c04def5b8161bb9d846d32a1de5b5acab82d290
Vitzxzz <vitzxzz@gmail.com>
2026-08-21 11:44:03 -0300

    atividade comparação de números 21-08

--- Programação WEB/Aula 3/Introdução ao JS/js/index.js
+++ Programação WEB/Aula 3/Introdução ao JS/js/index.js
@@ -1,12 +1,20 @@
-console.log("Olá js")
-
-const inputnome = document.getElementById("inputnome");
-const inputsenha = document.getElementById("inputsenha");
+const iptNum1 = document.getElementById("iptNum1");
+const iptNum2 = document.getElementById("iptNum2");
 const btnenviar = document.getElementById("btnenviar");
+const maiorNUM = document.getElementById("maiorNUM");
+const num1 = document.getElementById("num1");
+const num2 = document.getElementById("num2");
 
 function cliquei(){
-    console.log(inputnome.value, inputsenha.value)
+    console.log(iptNum1.value, iptNum2.value)
+    if(iptNum1.value>iptNum2.value){
+       maiorNUM.innerHTML = iptNum1.value
+       num1.classList.toggle('definecor');
+    }
+    else{
+        maiorNUM.innerHTML = iptNum2.value
+        num2.classList.toggle('definecor');
+    }
 }
 
 btnenviar.onclick = cliquei
-
