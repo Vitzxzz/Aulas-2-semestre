a9a2ac5698f257355f867d6d2b86b7cfb378b31b
Vitor Bonfim <vitzxzz@gmail.com>
2026-08-28 21:47:31 -0300

    js finalizado
    agora todo o texto vem injetado do js, não vindo do html mais.

--- Programação WEB/Aula 28-08/json/codigos/card.js
+++ Programação WEB/Aula 28-08/json/codigos/card.js
@@ -2,32 +2,53 @@ console.log("Está funcionando")
 
 let id_aplicativo = [
     {
-        nome: "Spotify",
+        nome: "SPOTIFY",
         descricao: "Um aplicativo para ouvir músicas e podcasts.",
+        imagem:"../imagens/spotify.png"
     },
     {
-        nome: "Whatsapp",
+        nome: "WHATSAPP",
         descricao: "Um aplicativo para se comunicar via texto ou ligação.",
+        imagem:"../imagens/whatsapp.png"
     },
     {
-        nome: "Snapchat",
+        nome: "SNAPCHAT",
         descricao: "Um aplicativo para interagir com amigos e utilizar filtros.",
+        imagem: "../imagens/snapchat.png"
     },
     {
-        nome: "Ifood",
+        nome: "IFOOD",
         descricao: "Um aplicativo para pedir comida via delivery.",
+        imagem:"../imagens/ifood.png"
     }
 ]
-
-let inserirDados = id_aplicativo.map(n => `<label>${n.descricao}</label>`).join("")
-
-console.log(inserirDados)
 
 const desc_spot = document.getElementById("desc-spot")
 const desc_whats = document.getElementById("desc-whats")
 const desc_snap = document.getElementById("desc-snap")
 const desc_ifood = document.getElementById("desc-ifood")
-desc_spot.id_aplicativo[0].innerHTML = inserirDados
-desc_whats.id_aplicativo[1].innerHTML = inserirDados
-desc_snap.innerHTML = inserirDados
-desc_ifood.innerHTML = inserirDados
+
+desc_spot.innerHTML = id_aplicativo[0].descricao
+desc_whats.innerHTML = id_aplicativo[1].descricao
+desc_snap.innerHTML = id_aplicativo[2].descricao
+desc_ifood.innerHTML = id_aplicativo[3].descricao
+
+const title_spot = document.getElementById("title-spot")
+const title_whats = document.getElementById("title-whats")
+const title_snap = document.getElementById("title-snap")
+const title_ifood = document.getElementById("title-ifood")
+
+title_spot.innerHTML = id_aplicativo[0].nome
+title_whats.innerHTML = id_aplicativo[1].nome
+title_snap.innerHTML = id_aplicativo[2].nome
+title_ifood.innerHTML = id_aplicativo[3].nome
+
+const img_spot = document.getElementById("img-spot")
+const img_whats = document.getElementById("img-whats")
+const img_snap = document.getElementById("img-snap")
+const img_ifood = document.getElementById("img-ifood")
+
+img_spot.src = id_aplicativo[0].imagem
+img_whats.src = id_aplicativo[1].imagem
+img_snap.src = id_aplicativo[2].imagem
+img_ifood.src = id_aplicativo[3].imagem
