96ce3e646a3f1f911e739d94ed7ca289b97e9e96
Vitor Bonfim <vitzxzz@gmail.com>
2026-09-01 21:51:16 -0300

    fazendo exercícios de matriz

---
+++ Programação imperativa/EXERCÍCIOS GERAIS/exercicios matriz.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+//EXERCÍCIOS MATRIZ
+
+//Declare uma matriz 3x3, preencha com valores pedidos ao usuário (usando dois for aninhados com scanf) e imprima a matriz formatada em linhas e colunas.
+/*
+int main(){
+    int i, j;
+    int m[3][3];
+    
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            printf("Digite para a posição: [%d], [%d].\n", i, j);
+            scanf("%d", &m[i][j]);
+        }
+    }
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            printf("%d\t", m[i][j]);
+        }
+        printf("\n");
+    }
+}
+*/
+
+//Crie uma matriz 2x4 já inicializada no código e calcule a soma de todos os seus elementos.
+/*
+int main(){
+    int i, j;
+    int m[2][4] = {{1, 2, 3, 4},{5, 6, 7, 8}};
+    int soma = 0;
+    
+    for(i=0; i<2; i++){
+        for(j=0;j<4;j++){
+          soma += m[i][j];  
+        }
+    }
+    printf("Soma: %d", soma);
+}
+*/
+
+
+
