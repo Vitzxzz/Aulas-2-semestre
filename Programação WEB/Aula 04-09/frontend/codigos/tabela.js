031886820490d57d624fe443a617c413ead5473e
Vitzxzz <vitzxzz@gmail.com>
2026-09-04 08:29:10 -0300

    criando pasta do dia 04-09

---
+++ Programação WEB/Aula 04-09/frontend/codigos/tabela.js
@@ -0,0 +1,32 @@
+console.log("Está funcionando")
+
+let id_aplicativo = [
+    {
+        nome: "Spotify",
+        descricao: "Um aplicativo para ouvir músicas e podcasts.",
+    },
+    {
+        nome: "Whatsapp",
+        descricao: "Um aplicativo para se comunicar via texto ou ligação.",
+    },
+    {
+        nome: "Snapchat",
+        descricao: "Um aplicativo para interagir com amigos e utilizar filtros.",
+    },
+    {
+        nome: "Ifood",
+        descricao: "Um aplicativo para pedir comida via delivery.",
+    }
+]
+
+let inserirDados = id_aplicativo.map(n => `
+        <tr>
+            <td>${n.nome}</td>
+            <td>${n.descricao}</td>
+        </tr>
+    `).join("")
+
+console.log(inserirDados)
+
+const tabelaCorpo = document.getElementById("corpo_tabela")
+tabelaCorpo.innerHTML = inserirDados
